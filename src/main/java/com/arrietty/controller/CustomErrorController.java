4867f830c16417d64b628928f3ca741653f83279
robert.zhang <yz3919@nyu.edu>
2022-04-21 11:25:44 -0400

    added custom error page

---
+++ src/main/java/com/arrietty/controller/CustomErrorController.java
@@ -0,0 +1,43 @@
+package com.arrietty.controller;
+
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return new ModelAndView("404.html");
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return new ModelAndView("500.html");
+            }
+        }
+
+        return new ModelAndView("500.html");
+    }
+
+    @RequestMapping("/401")
+    public ModelAndView get401(Model model){
+        return new ModelAndView("401.html");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
